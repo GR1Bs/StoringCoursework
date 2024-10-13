5b62e8aa188ace7f05fc095c7f9f9c628bb3008f
GR1Bs <lesenkov898@gmail.com>
2024-10-13 14:45:49 +0300

    Nachalo

---
+++ src/test/java/org/example/webjava/WebJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
