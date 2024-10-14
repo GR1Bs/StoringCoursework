5b24627cc48f5701909ab95c49e1cc2f43843287
GR1Bs <lesenkov898@gmail.com>
2024-10-14 14:54:22 +0300

    Табляца бд для препода

---
+++ src/main/java/org/example/webjava/Teacher/Teacher.java
@@ -0,0 +1,13 @@
+package org.example.webjava.Teacher;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Teacher {
+    @Id
+    private long id;
+    private String email;
+    private String password;
+    private String roll;
+}
